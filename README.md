3d17045d89069c0ef57856b28cddfdbe7a56b77f
David Busse <dpb@essub.com>
2012-02-12 17:35:11 -0600

    add ability run the script with command line arguments

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 name-generator is a super simple ruby script for generating compound names from a file of words.  At this point, the script prints out all possible 2 word combinations from the list of words.  I created the script to generate possible domain names from a list of keywords.
 
+Run the script with 1 or more filenames as arguments like so:
+```
+name-generator.rb [options] filename1 filename2 ...
+```
 
--- name-generator.rb
+++ name-generator.rb
@@ -1,12 +1,33 @@
+#!/usr/bin/env ruby
+$doc = <<ENDDOC
+name-generator.rb - Print all 2 word combinations from a file of keywords.
+
+SYNOPSIS
+name-generator.rb [options] keyword-filenames
+
+DESCRIPTION
+Given one or more files of keywords (one keyword on each line), this script will print out all possible 2 word combinations.
+
+OPTIONS
+  -h, --help
+  Print this help and exit.
+ENDDOC
+
+require 'getoptlong'
+
 class NameGenerator
-  def initialize(filename)
-    read_files(filename)
+  def initialize(filenames)
+    read_files(filenames)
   end
 
-  def read_files(filename)
+  def read_files(filenames)
     @words = []
-    File.open(filename, "r").each_line do |line|
-      @words << line.chomp
+
+    filenames.each do |f|
+      File.open(f, "r").each_line do |line|
+        word = line.chomp
+        @words << word unless @words.include?(word)
+      end
     end
   end
 
@@ -23,5 +44,54 @@ class NameGenerator
   end    
 end
 
-ng = NameGenerator.new("example_keywords.txt")
-ng.run
+def check_options 
+  opts = GetoptLong.new(
+	[ '--help', '-h', GetoptLong::NO_ARGUMENT ])
+
+  opts.quiet = true
+
+	opts.each do |opt, arg|
+		case opt
+		when '-h', '--help'
+			usage
+			exit
+		end
+	end
+end
+
+def get_args
+	if ARGV.length < 1
+		puts "Wrong number of arguments.\n\n"
+		usage
+		exit 1
+	end 
+	ARGV
+end
+
+def usage
+  puts $doc
+end
+
+
+if __FILE__ == $0
+
+  check_options	
+
+  filenames = get_args
+
+  errors = []
+  filenames.each do |f|
+    if !File.file? f
+      errors << "No such file - #{f}"
+    end
+  end
+
+  unless errors.empty?
+    puts errors.join("\n")
+    exit 1
+  end
+
+  generator = NameGenerator.new(get_args)
+  generator.run
+
+end
